42597d2aa54d9f8c5c98fd51f2fe41faf48e03d8
Nilesh kadam <nilesh.kadam222@gmail.com>
2020-11-25 19:01:32 +0530

    Range Date picker in angular 10

---
+++ Angular 10/RangeDatePickerExample/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule
+  ],
+  exports :[],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
